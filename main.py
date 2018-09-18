c8032549eed4498835a109620315f6efd6ef6cef
jts3444 <jts3444@hotmail.com>
2018-09-18 14:19:00 -0700

    started main.py, set up blank templates

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildablog@localhost:8888/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(360))
+    completed = db.Column(db.Boolean)
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+        self.completed = False
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        title_name = request.form['title']
+        body_text = request.form['body']
+        new_post = Blog(title_name, body_text)
+        db.session.add(new_post)
+        db.session.commit()
+
+    blogs = Blog.query.filter_by(completed=False).all()
+    completed_tasks = Blog.query.filter_by(completed=True).all()
+    return render_template('newpost.html',title="New Blog Post", 
+        blogs=blogs, completed_tasks=completed_tasks)
+
+
+
+if __name__ == '__main__':
+    app.run()
